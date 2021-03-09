b8ce849e0d96276826b2aef3828ab2a962fa03b7
Zac <79492867+SoggyRag@users.noreply.github.com>
2021-03-09 12:28:30 -0700

    Update comments on ex_05_05
    Also testing new branch and pull request

--- ex_06_05.py
+++ ex_06_05.py
@@ -1,7 +1,9 @@
+#combined all steps into one
 str = 'X-DSPAM-Confidence: 0.8475 '
 num = float(str[str.find(':')+1:])
 print(num)
 
+#separated steps to understand order better
 str = 'X-DSPAM-Confidence: 0.8475 '
 pos = str.find(':')
 npos = str[pos+1:]
