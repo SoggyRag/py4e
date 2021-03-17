38f9fe184c2064ce75635070c3e5411ce35cc258
Zac <79492867+SoggyRag@users.noreply.github.com>
2021-03-17 11:44:09 -0600

    Using dict() and tuples
    Exercise 2: This program counts the distribution of the hour of the day for each of the messages. You can pull the hour from the "From" line by finding the time string and then splitting that string into parts using the colon character. Once you have accumulated the counts for each hour, print out the counts, one per line, sorted by hour as shown below.
    
    Sample Execution:
    
    python timeofday.py
    Enter a file name: mbox-short.txt
    04 3
    06 1
    07 1
    09 2
    10 3
    11 6
    14 1
    15 2
    16 4
    17 2
    18 1
    19 1

--- ex_10_01.py
+++ ex_10_01.py
@@ -1,3 +1,12 @@
+#Exercise 1: Revise a previous program as follows: Read and parse the
+#"From" lines and pull out the addresses from the line. Count the number
+#of messages from each person using a dictionary.
+#After all the data has been read, print the person with the most commits
+#by creating a list of (count, email) tuples from the dictionary.
+#Then sort the list in reverse order and print out the person who
+#has the most commits.
+
+
 name = input('Enter a file name: ')
 counts = dict()
 try:
